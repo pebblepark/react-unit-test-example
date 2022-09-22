f635bd884cf3ed8a4c7054027ac93bd26831f1e1
박수정 <crystalpark@sqisoft.com>
2022-09-22 15:21:43 +0900

    컴포넌트 렌더링 테스트

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App, { add } from './App';
+import ReactDOM from 'react-dom';
+import App, { add, Login } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
@@ -22,5 +23,47 @@ describe('add function', () => {
       // 함수의 출력과 예상 결과 비교
       expect(result).toEqual(expected);
     });
+  });
+});
+
+describe('Login component tests', () => {
+  let container: HTMLDivElement;
+
+  // beforeEach : 이 파일의 각 테스트가 실행되기 전에 해당 함수 실행
+  // 함수가 promise를 반환하거나 생성자인 경우, jest는 테스트를 실행하기 전에 해당 promise가 해결될 때까지 대기
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    ReactDOM.render(<Login />, container);
+  });
+
+  // afterEach : 테스트가 서로 방해되지 않도록 각 테스트가 마무리 되었을 때 초기화
+  afterEach(() => {
+    document.body.removeChild(container);
+    container.remove();
+  });
+
+  // 각 입력 필드 렌더링 테스트
+  it('Renders all input fields correctly', () => {
+    // 입력 필드 선택
+    // eslint-disable-next-line testing-library/no-node-access
+    const inputs = container.querySelectorAll('input');
+    // 입력 필드 올바르게 렌더링 되었는지 확인
+    expect(inputs).toHaveLength(2);
+
+    // 첫 번째, 두 번째 입력 필드 이름이 각각 '이메일', '비밀번호' 인지 확인
+    expect(inputs[0].name).toBe('email');
+    expect(inputs[1].name).toBe('password');
+  });
+
+  // 각 버튼 렌더링 테스트
+  it('Renders all buttons correctly', () => {
+    // eslint-disable-next-line testing-library/no-node-access
+    const buttons = container.querySelectorAll('button');
+    expect(buttons).toHaveLength(2);
+
+    expect(buttons[0].type).toBe('button');
+    expect(buttons[1].type).toBe('button');
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,24 @@ export function add(a: number, b: number) {
   return a + b;
 }
 
+export function Login() {
+  return (
+    <div>
+      <div>
+        <input type="email" name="email" placeholder="email" />
+      </div>
+      <div>
+        <input type="password" name="password" placeholder="password" />
+      </div>
+
+      <div>
+        <button type="button">Sign In</button>
+        <button type="button">Sign Up</button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
